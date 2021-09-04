3be8a81155c74435430d20d30f6097dc90646a70
HariKrishna-28 <harikrishnac2002@gmail.com>
2021-09-04 22:07:17 +0530

    posted weather data in client

--- client/src/FormInfo.jsx
+++ client/src/FormInfo.jsx
@@ -1,29 +1,32 @@
 import React, { useState } from 'react'
 import axios from "axios"
 
-const baseUrl = "http://localhost:5000/"
+const baseUrl = "http://localhost:5000/weather"
 
 const FormInfo = () => {
     const [data, setData] = useState([])
 
     async function handleDetails(e) {
+        e.preventDefault()
         console.log("clicked handle")
-        await axios.post(baseUrl).then(res => {
-            setData(res.data)
-            console.log(data);
-        }
-        )
+        axios.get(baseUrl).then(res => {
+            // setData(res.data)
+            console.log(res.data);
+
+        })
     }
 
     return (
         <>
 
             <div className="flex justify-center items-center h-screen bg-black text-white">
+                {/* <h1 className="flex font-semibold text-3xl">Weather App</h1> */}
                 <div className="container p-10 rounded bg-purple-800 text-white mx-10 lg:mx-48">
-                    <form action={baseUrl} method="POST">
+
+                    <form >
                         <div className="grid grid-cols-1 gap-4">
                             <div className="flex justify-center align-center pt-2">
-                                <label className="text-2xl font-semibold">Country Name</label>
+                                <label className="text-2xl font-semibold">Enter Country Name</label>
                             </div>
                             <input
                                 type='text'
@@ -33,10 +36,16 @@ const FormInfo = () => {
                                 required />
                         </div>
                         <div className="flex justify-center align-center">
-                            <button className="bg-green-700 border-2 border-yellow-500 hover:bg-green-600 w-auto px-4 mt-4 py-1 rounded"
-                                type="submit" onClick={(e) => handleDetails()}>Submit</button>
                         </div>
                     </form>
+                    <div className="flex">
+                        <button
+                            className="bg-green-700 hover:bg-green-600 w-auto px-5 mt-6 py-2 font-semibold rounded"
+                            type="submit"
+                            onClick={(e) => handleDetails(e)}>
+                            Submit
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 
 const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
 const https = require('https')
 const bodyParser = require('body-parser')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(express.json())
+app.use(cors())
 
 
 app.get("/", (req, res) => {
@@ -14,45 +17,51 @@ app.get("/", (req, res) => {
     res.send("Server running")
 })
 
-app.post("/", (req, res) => {
-    // console.log("received")
-    // res.send()
-    const query = req.body.cityName
-    const appId = "c2f2c006d14606aa404898a8efd5beb6"
-    const units = "metric"
+app.get("/weather", async (req, res) => {
 
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appId}&units=${units}`
+    try {
+        console.log("received")
+        // res.send()
+        const query = "belgium"
+        const appId = "c2f2c006d14606aa404898a8efd5beb6"
+        const units = "metric"
 
+        // const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appId}&units=${units}`
 
-    https.get(URL, (response) => {
-        console.log(response.statusCode)
-
-        response.on('data', (data) => {
-            const weatherData = JSON.parse(data)
-            console.log(weatherData);
-            res.json(weatherData)
-
-
-
-
-            // res.send(data)
-            // const weatherData = JSON.parse(data)
-            // const weatherTemp = weatherData.main.temp
-            // const weatherDescription = weatherData.weather[0].description
-            // const weatherIcon = weatherData.weather[0].icon
-            // const imageUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
-
-            // res.write("<h1>Weather data </h1>")
-            // res.write(`<img src = ${imageUrl} alt = "image" />`)
-            // res.write(`<p>The weather is currently ${weatherDescription} in ${query} </p>`)
-            // res.write(`<p> The temperature in ${query} is ${weatherTemp}  </p>`)
-            // res.send()
+        let config = {
+            method: 'get',
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appId}&units=${units}`,
+            headers: {
+                'accept': 'application/json'
+            }
+        }
+        const response = await axios(config)
+        console.log(response);
+        res.json(
+            response.data
+        )
+    } catch (error) {
+        console.log(error.message)
+        res.json({
+            success: false,
+            message: "server error"
         })
-    })
+    }
 })
 
 
 
+// console.log(res);
+
+// https.get(URL, (response) => {
+//     response.once('data', (data) => {
+//         const weatherData = JSON.parse(data)
+//         console.log(weatherData);
+//         res.json(weatherData)
+//         res.send(data)
+//     })
+
+// })
 app.listen(process.env.PORT || 5000, () => {
     console.log('Server running at port 5000!')
 })
